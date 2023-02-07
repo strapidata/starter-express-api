b647746c53287482183bfce040b86189939b1411
strapidata <91236433+strapidata@users.noreply.github.com>
2023-02-07 00:33:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
+    
+    fetch("https://open-book-house.herokuapp.com/libros/?id=999")
+  .then((response) => {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log("Unable to fetch -", err);
+  });
+    
     console.log("Just got a request!")
     res.send('MCUADROS!')
 })
